38ab0862981340f26a3b7d8207de96f23346d6e7
Eliezer Castro <smallcity.programmer@gmail.com>
2023-10-31 23:33:22 -0300

    Adicionando MovieCard

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,11 @@
+import {Link} from 'react-router-dom'
+import {FaStart} from 'react-icons/fa'
+
+
+const MovieCard = () => {
+  return (
+    <div>MovieCard</div>
+  )
+}
+
+export default MovieCard
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = ( )=> {
     <div className="container">
       <h2 className="title">Melhores filmes:</h2>
       <div className="movies-container">
-        {topMovies.length === 0 && <p>Carregando</p> }
+        {topMovies.length === 0 && <p>Carregando...</p> }
       { topMovies.length > 0  && topMovies.map((movie) => (<p key={movie.id}>{movie.title}</p>
       ))}
       </div>
