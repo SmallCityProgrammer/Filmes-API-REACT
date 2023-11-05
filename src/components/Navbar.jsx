eba7ee0d2e37d179ac933ddfd2586d8b2ba7352b
Eliezer Castro <smallcity.programmer@gmail.com>
2023-11-04 22:40:14 -0300

    criando search

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,28 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import {BiCameraMovie, BiSearchAlt2} from 'react-icons/bi';
 import "./Navbar.css"
 
 function Navbar() {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(search);
+  }
+  
+
  return(  
     <div>
       <nav id='navbar'>
         <h2>
         <Link to='/'><BiCameraMovie />MoviesLib</Link>
         </h2>
-        <form>
-          <input type="text" placeholder="Busque um filme" />
+        <form onSubmit={handleSubmit}>
+          <input type="text" 
+          placeholder="Busque um filme" 
+          onChange={(e) => setSearch(e.target.value)} />
           <button type="submit">
             <BiSearchAlt2 />
           </button>
