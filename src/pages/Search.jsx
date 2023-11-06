59cd66ee7d362c5f13d1edbf30bf2b3ed2c6337a
Eliezer Castro <smallcity.programmer@gmail.com>
2023-11-05 22:26:40 -0300

    ...

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -5,11 +5,17 @@ import MovieCard from "../components/MovieCard";
 const searchURL = import.meta.env.VITE_SEARCH;
 const apiKey = import.meta.env.VITE_API_KEY;
 
+import './MoviesGrid.css'
+
 const Search = ( )=> {
-  return(
-    <>
-    Search
-    </>
+  return(    
+  <div className="container">
+    <h2 className="title">Melhores filmes:</h2>
+    <div className="movies-container">
+      {topMovies.length === 0 && <p>Carregando...</p> }
+      {topMovies.length > 0  && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+    </div>
+</div>
   )
 }
 
