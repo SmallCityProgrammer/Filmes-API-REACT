5984dd278c305392f46424beeaf18bc9548d1ae0
Eliezer Castro <smallcity.programmer@gmail.com>
2023-11-08 22:49:37 -0300

    pequisa funcionando

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,43 +1,52 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import MovieCard from "../components/MovieCard";
 
 const searchURL = import.meta.env.VITE_SEARCH;
 const apiKey = import.meta.env.VITE_API_KEY;
 
-import './MoviesGrid.css';
+import "./MoviesGrid.css";
 
-const Search = ()=> {
-  const [searchParams] = useSearchParams()
+const Search = () => {
+  const [searchParams] = useSearchParams();
 
-  const [movies, setMovies] = useState([])
-  const query = searchParams.get('q')
+  const [movies, setMovies] = useState([]);
+  const query = searchParams.get("q");
 
-  const getTopSearchedMovies = async (url) => {
-    const res = await fetch(url);
-    const data = await res.json();
-
-    setTopMovies(data.results)
-    console.log(data.results)
-  }
+  const getSearchedMovies = async (url) => {
+    try {
+      const res = await fetch(url);
+      if (!res.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await res.json();
+      setMovies(data.results);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
   useEffect(() => {
-    
-    const searchWithQueryURL = `${searchURL}?${apiKey}&query=${query}`
-    
-    getTopSearchedMovies(searchWithQueryURL);
-  }, []);
+    if (query) {
+      const searchWithQueryURL = `${searchURL}?${apiKey}&query=${query}`;
+      getSearchedMovies(searchWithQueryURL);
+    }
+  }, [query]);
 
-  return(    
-  <div className="container">
-    <h2 className="title">Resultados para: <span className="query-text">{query}</span></h2>
-    <div className="movies-container">
-      {movies.length === 0 && <p>Carregando...</p> }
-      {movies.length > 0  && 
-        topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+  return (
+    <div className="container">
+      <h2 className="title">
+        Resultados para: <span className="query-text">{query}</span>
+      </h2>
+      <div className="movies-container">
+        {movies.length > 0 ? (
+          movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)
+        ) : (
+          <p>No movies found.</p>
+        )}
+      </div>
     </div>
-</div>
-  )
-}
+  );
+};
 
 export default Search;
