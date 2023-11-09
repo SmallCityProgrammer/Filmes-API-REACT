174a961033475ca024f93239eaca804d25def92f
Eliezer Castro <smallcity.programmer@gmail.com>
2023-11-08 22:54:33 -0300

    movie.jsx

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,3 +1,17 @@
+import {useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+import{
+  BsGraphUp,
+  BsWallet2,
+  BsHourglassSplit,
+  BsFillFileEarmarkTextFill
+} from 'react-icons/bs';
+
+import MovieCard from "../components/MovieCard";
+
+import './Movie.css';
+
+
 const Movies = ( )=> {
   return(
     <>
