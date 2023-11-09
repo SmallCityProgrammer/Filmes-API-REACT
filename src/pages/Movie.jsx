049f744c95e8ca40b7389894686b9f42d332390c
Eliezer Castro <smallcity.programmer@gmail.com>
2023-11-08 23:21:37 -0300

    Ajeitando currency para USD formatação tocalestring

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -26,6 +26,13 @@ const Movie = () => {
     setMovie(data)
   }
 
+  const formatCurrency = (number) => {
+    return number.toLocaleString("en-US", {
+      style:"currency",
+      currency:"USD"
+    })
+  }
+
   useEffect(() => {
     const movieUrl = `${moviesURL}${id}?${apiKey}`;
     getMovie(movieUrl)
@@ -39,25 +46,25 @@ const Movie = () => {
         <p className="tagline">{movie.tagline}</p>
         <div className='info'>
           <h3>
-            <BsWallet2 /> Orçamento
+            <BsWallet2 /> Orçamento:
           </h3>
-          <p>{movie.budget}</p>
+          <p>{formatCurrency(movie.budget)}</p>
         </div>
         <div className='info'>
           <h3>
-            <BsGraphUp /> Receita
+            <BsGraphUp /> Receita:
           </h3>
-          <p>{movie.revenue}</p>
+          <p>{formatCurrency(movie.revenue)}</p>
         </div>
         <div className='info'>
           <h3>
-            <BsHourglassSplit /> Duração
+            <BsHourglassSplit /> Duração:
           </h3>
           <p>{movie.runtime}</p>
         </div>
         <div className='info'>
           <h3>
-            <BsFillFileEarmarkTextFill /> Descrição
+            <BsFillFileEarmarkTextFill /> Descrição:
           </h3>
           <p>{movie.overview}</p>
         </div>
